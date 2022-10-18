0b498775d55a19db78b367649a3027da4244ffd4
Andrey G <a.gavrilov@promo-bot.ru>
2022-10-18 17:57:42 +0500

    update style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*html"],
+  content: ["./*.html"],
   theme: {
     screens: {
       sm: "480px",
       md: "768px",
       lg: "976px",
-      lg: "1440px",
+      xl: "1440px",
     },
     extend: {
       colors: {
